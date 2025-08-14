7b072d848d4b8b08b2cb1597f33e6675fe77344a
Micheli Melki <michelimelki@gmail.com>
2025-08-14 11:36:43 -0300

    feat: setup nest with swagger

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdService } from './data/services/prod.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('CarProtect')
+  .setDescription('Projeto CarProtect')
+  .setContact("CarProtect","https://github.com/Grupo-05-Turma-JavaScript-08","grupo.05.turma.javascript.08@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
